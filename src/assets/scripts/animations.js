eeba48a5b41d1c92f96293b08e6bdeced8d5ef06
fraz <fraz@o2.pl>
2025-05-07 22:35:48 +0200

    remove obsolete load event listener

--- src/assets/scripts/animations.js
+++ src/assets/scripts/animations.js
@@ -3,42 +3,38 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-window.addEventListener("load", init);
+gsap.set("[data-fade]", { autoAlpha: 0, y: 20 });
+ScrollTrigger.batch("[data-fade]", {
+	batchMax: 3,
+	onEnter: (batch) => gsap.to(batch, { autoAlpha: 1, y: 0, stagger: 0.15 }),
+});
 
-function init() {
-	gsap.set("[data-fade]", { autoAlpha: 0, y: 20 });
-	ScrollTrigger.batch("[data-fade]", {
-		batchMax: 3,
-		onEnter: (batch) => gsap.to(batch, { autoAlpha: 1, y: 0, stagger: 0.15 }),
-	});
+gsap.set("[data-splash]", {
+	autoAlpha: 0,
+	xPercent: window.innerWidth >= 800 ? -10 : 0,
+	y: window.innerWidth < 800 ? 20 : 0,
+});
+gsap.to("[data-splash]", {
+	autoAlpha: 1,
+	xPercent: 0,
+	y: 0,
+	duration: 1,
+});
 
-	gsap.set("[data-splash]", {
-		autoAlpha: 0,
-		xPercent: window.innerWidth >= 800 ? -10 : 0,
-		y: window.innerWidth < 800 ? 20 : 0,
-	});
-	gsap.to("[data-splash]", {
+let mm = gsap.matchMedia();
+
+mm.add("(min-width: 601px)", () => {
+	gsap.set("[data-slide] img", { x: "15vw", autoAlpha: 0 });
+	gsap.to("[data-slide] img", {
 		autoAlpha: 1,
-		xPercent: 0,
-		y: 0,
-		duration: 1,
 	});
-
-	let mm = gsap.matchMedia();
-
-	mm.add("(min-width: 601px)", () => {
-		gsap.set("[data-slide] img", { x: "15vw", autoAlpha: 0 });
-		gsap.to("[data-slide] img", {
-			autoAlpha: 1,
-		});
-		gsap.to("[data-slide] img", {
-			scrollTrigger: {
-				trigger: "[data-slide] img",
-				scrub: 1,
-				start: "top bottom",
-				end: "bottom top",
-			},
-			x: "-15vw",
-		});
+	gsap.to("[data-slide] img", {
+		scrollTrigger: {
+			trigger: "[data-slide] img",
+			scrub: 1,
+			start: "top bottom",
+			end: "bottom top",
+		},
+		x: "-15vw",
 	});
-}
+});
